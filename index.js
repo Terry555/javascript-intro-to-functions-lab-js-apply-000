bd75fb850eacf1f52b750e48e16107a517d74e53
Terry Foley <terry.foley555@gmail.com>
2018-05-29 01:00:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,4 +23,8 @@ function sayHiToGrandma(string) {
   if(string.toUpperCase()===string) {
   return "YES INDEED!"
 }
+
+  if("I love you, Grandma."===string) {
+    return "I love you, too."
+  }
 }
