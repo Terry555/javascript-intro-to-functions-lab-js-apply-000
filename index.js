104192420ab62c3e60f1582d3d0f23c955df4caa
Terry Foley <terry.foley555@gmail.com>
2018-05-29 00:57:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string.toLowerCase()===string)
+  if(string.toLowerCase()===string) {
     return "I can't hear you!"
 }
 
 
   if(string.toUpperCase()===string) {
   return "YES INDEED!"
+}
 }
